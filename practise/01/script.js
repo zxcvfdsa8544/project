d9dc8b9a1c5f8e7416220d2e33e7b5331facc3ea
fuyu <zxcvfdsa8544@gmail.com>
2026-08-18 17:04:28 +0800

    practise03 使用物件簡化(未完成)

--- practise/01/script.js
+++ practise/01/script.js
@@ -1,4 +1,4 @@
-let sum = 0;
+let totalPrice = 0;
 const blueberry = document.getElementById("blueberry");
 const strawbreey = document.getElementById("strawberry");
 const pig = document.getElementById("pig");
@@ -6,26 +6,52 @@ const total = document.getElementById("total");
 const clear = document.getElementById("clear");
 
 blueberry.onclick = () => {
-    sum += parseInt(blueberry.value);
-    console.log(`目前購物車總額 : ${sum}`);
+    cart.productNum++;
+    cart.product1++;
+    cart.totalPrice += parseInt(blueberry.value);
+    showCart();
 }
 strawbreey.onclick = () => {
-    sum += parseInt(strawbreey.value);
-    console.log(`目前購物車總額 : ${sum}`);
+    cart.productNum++;
+    cart.product2++;
+    cart.totalPrice += parseInt(strawbreey.value);
+    showCart();
 }
 
 pig.onclick = () => {
-    sum += parseInt(pig.value);
-    console.log(`目前購物車總額 : ${sum}`);
+    cart.productNum++;
+    cart.product3++;
+    cart.totalPrice += parseInt(pig.value);
+    showCart();
 }
 
 total.onclick = () =>{
-    console.log(`目前購物車總額 : ${sum}`);
+    showCart();
 }
 
 clear.onclick = () =>{
-    sum = 0;
+    cart.productNum = 0;
+    cart.product1 = 0;
+    cart.product2 = 0;
+    cart.product3 = 0;
+    cart.totalPrice = 0;
     console.log(`已清空購物車`);
-    console.log(`目前購物車總額 : ${sum}`);
+    showCart();
+}
+
+function showCart(){
+    console.log(`目前商品數 : ${cart.productNum}`)
+    console.log(`商品1 : ${cart.product1}`)
+    console.log(`商品2 : ${cart.product2}`)
+    console.log(`商品3 : ${cart.product3}`)
+    console.log(`目前購物車總額 : ${cart.totalPrice}`)
+}
+
+let cart = {
+    productNum: 0,
+    product1: 0,
+    product2: 0,
+    product3: 0,
+    totalPrice: 0
 }
 
